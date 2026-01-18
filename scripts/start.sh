8d6a44d1d0dbe8200428c0d03125bda48221f45e
Simon Röhrl <simon.roehrl01@gmail.com>
2026-01-18 20:34:23 +0100

    fix starting scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,4 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 EXTENSION_PATH="$SCRIPT_DIR/../extension"
 
-../chromium/src/out/Default/Chromium.app/Contents/MacOS/Chromium \
-  --load-extension="$EXTENSION_PATH" \
-  ../eval/sample.html 2> output
+./chromium/src/out/Default/Chromium.app/Contents/MacOS/Chromium $(realpath ./eval/sample.html) 2> output
